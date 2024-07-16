000d1b7b174bd9eac4bcb412e7d02bbd59b933d2
Mtrikkyo <tsubasa.masumura@icoud.com>
2024-07-16 14:00:03 +0900

    Add:extracting and removing  functions

--- src/utils/download_wikitext103.sh
+++ src/utils/download_wikitext103.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 URL="https://dax-cdn.cdn.appdomain.cloud/dax-wikitext-103/1.0.1/wikitext-103.tar.gz"
+SAVEDIR="data"
+FILE="wikitext-103.tar.gz"
 
-FILE="data/wikitext-103.tar.gz"
-
+# download
 if [ -f "$FILE" ];then
     echo "already exist"
 else;
     # TODO tarファイルの保存先をオプションで指定できるようにしたい
-    wget -O "$FILE" "$URL"
+    wget -O "$SAVEDIR/$FILE" "$URL"
 fi
 
+# extract
+tar -xzC "$SAVEDIR" -f "$SAVEDIR/$FILE"
+
+# remove `tar`file
+rm "$SAVEDIR/$FILE"
