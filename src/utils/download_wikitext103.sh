bd779a85ee847d874a009b46203ad110d67796bc
Mtrikkyo <tsubasa.masumura@icoud.com>
2024-07-16 16:11:30 +0900

    Add:adding gitkeep function

--- src/utils/download_wikitext103.sh
+++ src/utils/download_wikitext103.sh
@@ -4,19 +4,24 @@ URL="https://dax-cdn.cdn.appdomain.cloud/dax-wikitext-103/1.0.1/wikitext-103.tar
 SAVEDIR="data"
 FILE="WikiText103"
 
-# download
-if [ -f "$SAVEDIR/$FILE.tar.gz" ];then
+if [ -f "$SAVEDIR/$FILE/.gitkeep" ];then
+    
     echo "already exist"
 else;
+    
+    # download
     # TODO tarファイルの保存先をオプションで指定できるようにしたい
     wget -O "$SAVEDIR/$FILE.tar.gz" "$URL"
+    
+    # extract
+    tar -xzC "$SAVEDIR" -f "$SAVEDIR/$FILE.tar.gz"
+    
+    # rename file
+    mv "$SAVEDIR/wikitext-103" "$SAVEDIR/$FILE"
+    
+    # remove `tar`file
+    rm "$SAVEDIR/$FILE.tar.gz"
+    
+    # make .gitkeep file
+    touch "$SAVEDIR/$FILE/.gitkeep"
 fi
-
-# extract
-tar -xzC "$SAVEDIR" -f "$SAVEDIR/$FILE.tar.gz"
-
-# rename file
-mv "$SAVEDIR/wikitext-103" "$SAVEDIR/$FILE"
-
-# remove `tar`file
-rm "$SAVEDIR/$FILE.tar.gz"
