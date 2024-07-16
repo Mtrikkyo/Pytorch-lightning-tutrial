551a546ab6b8900335fbd51f5c67965578f1c279
Mtrikkyo <tsubasa.masumura@icoud.com>
2024-07-16 19:08:29 +0900

    append path to custom script

--- src/models/toy_pl.py
+++ src/models/toy_pl.py
@@ -1,19 +1,49 @@
 #!.venv/bin/python3
 """train script with pytorch-lightning"""
+
+__version__ = "0.0"
+
+import os
+import sys
+from pathlib import Path
 from typing import *
+
 import torch.nn.functional as F
 import torch.optim as optim
 import lightning as L
-from .toy_model import ToyModel
+
+# const
+ROOT_DIR = Path.cwd()
+CUSTOM_SCRIPT_DIR = ROOT_DIR / "src"
+
+sys.path.append(str(CUSTOM_SCRIPT_DIR))
+from models import ToyModel
 
 
 class LitToyModel(L.LightningModule):
 
-    def __init__(self, num_class) -> None:
+    def __init__(
+        self,
+        in_channels: int = 3,
+        out_channels: int = 32,
+        num_class: int = 10,
+        kernel_size: int = 3,
+        stride: int = 1,
+        padding: int = 1,
+        dilation: int = 1,
+    ) -> None:
         self.num_class = num_class
         super().__init__()
 
-        self.model = ToyModel()
+        self.model = ToyModel(
+            in_channels,
+            out_channels,
+            num_class,
+            kernel_size,
+            stride,
+            padding,
+            dilation,
+        )
 
     def training_step(self, batch, batch_idx):
         x, y = batch
