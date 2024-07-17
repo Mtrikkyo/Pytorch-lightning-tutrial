4307487adfb51fb2669bdac2dca2fce28aabd7ba
Mtrikkyo <tsubasa.masumura@icoud.com>
2024-07-17 19:30:47 +0900

    Add:WikiText103 dataset class

--- src/utils/dataset/dataset_factory.py
+++ src/utils/dataset/dataset_factory.py
@@ -2,26 +2,55 @@
 """dataset class for WikiText103"""
 
 # import
+from pathlib import Path
+from typing import *
+
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset
 
 
 class WikiText103(Dataset):
+    def __init__(
+        self,
+        token_file: str | Path,
+        tokenizer: Any,
+    ) -> None:
+        super().__init__()
+
+        self.raw_data = Path(token_file).read_text().splitlines(True)
+        self.tokenizer = tokenizer
+
+        self.dataset = list(
+            tokenizer.convert_tokens_to_ids(
+                tokenizer.tokenize(
+                    line.strip(" ").replace("\n", "[SEP]").replace("<unk>", "[UNK]"),
+                    max_length=512,
+                    truncation=True,
+                )
+            )
+            for line in self.raw_data
+        )
+
+    def __len__(self) -> None:
+        return len(self.dataset)
+
+    def __getitem__(self, index):
+        return self.dataset[index]
+
     pass
 
 
 if __name__ == "__main__":
-    from transformers import BertTokenizer
+    from transformers import AutoTokenizer
     from pathlib import Path
+    from tqdm import tqdm
 
     ROOT_DIR = Path.cwd().parents[2]
     DATA_DIR = ROOT_DIR / "data/WikiText103"
 
-    # train_data = (DATA_DIR / "wiki.train.tokens").read_text()
-    valid_data = (DATA_DIR / "wiki.valid.tokens").read_text()
-    print(valid_data)
-
-    tokenizer = BertTokenizer.from_pretrained("bert-base-cased")
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-cased", force_download=True)
+    train_set = WikiText103(DATA_DIR / "wiki.valid.tokens", tokenizer)
+    print(train_set[:10])
 
     pass
